5a1b6bddb2c28e6102df7fd82f9c6330c8188384
Benjamin Landis <bmlandis2010@gmail.com>
2022-02-12 21:50:27 -0600

    Update pictures and text

--- pages/charity-donation.vue
+++ pages/charity-donation.vue
@@ -5,15 +5,39 @@
     </div>
     <div class="section details">
       <h1 class="title">Charity Donations</h1>
-      <p>Instead of flowers we would like to suggest people make a donation to one of the following charities:</p>
-      <h2>KC Pet Project</h2>
-      <!-- <p>We are so grateful to <em>Marie Curie</em> for their support and making sure their patients are as 
-        comfortable and peaceful as possible when the end came.
-      </p> -->
-      <a class="button" target="_blank" rel=”noopener” href="https://kcpetproject.org/donate/">Donate to KC Pet Project</a>
+
+      <p>Instead of flowers, please consider making a donation to one of the following charities that were important to Sean:</p>
+
+      <h2>Life Issues Addiction Recovery - Heartland Baptist Fellowship</h2>
+      <p>Life Issues is a Christ-centered addiction recovery program, whose mission statement is:
+        Helping, saving, and freeing the addicted, by the Spirit of the Lord (based on Luke 4:18-19)</p>
+      <a class="button" target="_blank" rel=”noopener” href="https://forms.ministryforms.net/viewForm.aspx?formId=e6a2df33-a9cf-4189-bf6b-7f162d869b9b">Donate to Life Issues Addiction Recovery</a>
+      <a class="button" target="_blank" rel=”noopener” href="http://www.lifeissuesonline.org/p/mission.html">Mission</a>
+            
+      <h2>Joshua House - Two Rules Ministrities</h2>
+      <p>Our mission is to help men discover, seek, and fulfill God's purpose for their lives,
+        and to teach them to fulfill their God given role as a man according to God's Word.
+        We provide housing at the Joshua House within a structured environment,
+        job acquisition opportunities, food, clothing, medical services, transportation, and spiritual guidance</p>
+      <a class="button" target="_blank" rel=”noopener” href="https://tworulesministries.org/contributions/">Donate to Two Rules Ministrities</a>
+
+      <h2>Union Gospel Mission of Salem, OR</h2>
+      <p>This year, 1,800 homeless men, women, and children will come through our doors seeking help.
+        Your support will provide hot meals, safe shelter, showers, employment services,
+        housing assistance, addictions recovery, and more</p>
+      <a class="button" target="_blank" rel=”noopener” href="https://ugmsalem.org/donate/">Donate to Union Gospel Mission of Salem, OR</a>
+
       <h2>Wayside Waifs</h2>
-      <!-- <p><em>Wayside Waifs</em>'s goal of giving joy to children during a terrible time is something that would be a fitting tribute.</p> -->
-      <a class="button" target="_blank" rel=”noopener” href="https://secure.waysidewaifs.org/site/Donation2?df_id=6146&mfc_pref=T&6146.donation=form1">Donate to Wayside Waifs</a>
+      <p>Your gift provides life-saving medical care, safe shelter, nutritious food, and love to more than 6,000 abandoned,
+        abused, and homeless pets each year.</p>
+      <a class="button" target="_blank" rel=”noopener” href="https://waysidewaifs.org/donate/">Donate to Wayside Waifs</a>
+
+      <h2>KC Pet Project</h2>
+      <p>Donations support our lifesaving mission by giving a gift to help Kansas City's pets.
+        KC Pet Project is the only shelter committed to saving the lives of homeless pets in Kansas City.
+        Your financial support provides medicine, veterinary care, vaccines, food, and resources to help us
+        save a record number of pets who have nowhere else to turn and to help keep pets and their people together.</p>
+      <a class="button" target="_blank" rel=”noopener” href="https://kcpetproject.org/donate/">Donate to KC Pet Project</a>
     </div>
   </div>
 </template>
